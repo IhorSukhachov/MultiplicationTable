2c5b24e94af27a286f36922fddff1064b6e69017
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-16 16:29:58 +0200

    declaring necessary variables

--- MultiplicationTable/ContentView.swift
+++ MultiplicationTable/ContentView.swift
@@ -7,18 +7,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+struct Question {
+    var firstMultiplier: Int
+    var secondMultiplier: Int
+    var correctAnswer: Int {
+        firstMultiplier * secondMultiplier
     }
 }
 
+struct ContentView: View {
+    @State private var gameIsActive: Bool
+    @State private var maxTable : Int
+    @State private var questionCount : Int
+    @State private var questions: [Question] = []
+    @State private var currentQuestion: Int = 0
+    @State private var userAnswer: Int? = nil
+    @State private var score = 0
+    
+    
+    var body: some View {
+        
+    }
+}
+
+/*
 #Preview {
     ContentView()
-}
+}*/
