32ecf88c70cbbd6663a91046427076becbc3967a
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-16 16:10:55 +0200

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Ihor Sukhachov on 16.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
